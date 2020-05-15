c0e1d0127bb3701d025e522bbfafc107a7a1ae80
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-05-15 13:43:08 -0400

    returns trail objects

--- swipe.js
+++ swipe.js
@@ -24,35 +24,37 @@ $(document).ready(function () {
         }
     })
 
-var trails =[]
+    var trails = []
 
-function srcCheck(){
-    if($('.trail-img').attr('src')==='' ){
-      $(this).attr('src','https://img.icons8.com/wired/64/000000/no-image.png')
+    function srcCheck() {
+        if ($('.trail-img').attr('src') === '') {
+            $(this).attr('src', 'https://img.icons8.com/wired/64/000000/no-image.png')
         }
     }
 
-    function getTrails(){
+    function getTrails() {
 
         $.ajax({
             url: 'https://www.hikingproject.com/data/get-trails?lat=35.9705&lon=-79.9974&maxDistance=10&key=200758271-3165bfaa7d6b0bedfbf0fcdfbad4ec16',
             method: 'GET'
-        }).then(function(response){
-            $('#trail1').attr('src',response.trails[0].imgSmallMed).attr('alt','trail')
-            $('#trail2').attr('src',response.trails[1].imgSmallMed).attr('alt','trail')
-            $('#trail3').attr('src',response.trails[2].imgSmallMed).attr('alt','trail')
-            $('#trail4').attr('src',response.trails[3].imgSmallMed).attr('alt','trail')
-            $('#trail5').attr('src',response.trails[4].imgSmallMed).attr('alt','trail')
-        srcCheck();    
-        $('.save-trail').click(function(){
-            trails.push((response.trails[$(this).attr('data-trail-index')].name))
-            localStorage.setItem('trails',JSON.stringify(trails))
+        }).then(function (response) {
+            $('#trail1').attr('src', response.trails[0].imgSmallMed).attr('alt', 'trail')
+            $('#trail2').attr('src', response.trails[1].imgSmallMed).attr('alt', 'trail')
+            $('#trail3').attr('src', response.trails[2].imgSmallMed).attr('alt', 'trail')
+            $('#trail4').attr('src', response.trails[3].imgSmallMed).attr('alt', 'trail')
+            $('#trail5').attr('src', response.trails[4].imgSmallMed).attr('alt', 'trail')
+            srcCheck();
+            $('.save-trail').click(function () {
+                trails.push(response.trails[$(this).attr('data-trail-index')])
+                localStorage.setItem('trails', JSON.stringify(trails))
+            })
         })
-    })
-}
+    }
+$('#test').click(function(){
+    alert(localStorage.getItem('trails'))
+})
 
-
-getTrails();
+    getTrails();
 
 
 
