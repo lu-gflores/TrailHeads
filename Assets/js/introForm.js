858d05552317f9c5251017adc1fc8eb3d8d08277
ryand67 <rwdevenney@gmail.com>
2020-05-15 21:40:11 -0400

    Card design progress

--- Assets/js/introForm.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("#zipDiv").hide();
-    $("#otherDiv").hide();
-    $(document).on("click", ".nextButton", function() {
-        event.preventDefault();
-        $(this).parent("div").next("div").show();
-        $(this).parent("div").hide();
-    })
-})
