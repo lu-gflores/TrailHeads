37b7f9dd56ee8a6110129fed269d92464495943c
George Flores <georgeflores886@gmail.com>
2020-05-16 21:32:42 -0400

    added event listener to search button

--- Assets/js/swipe.js
+++ Assets/js/swipe.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
     })
 
 
-// ----trail finder API
+// ----hiking project API
     function getTrails(a, b){
 
         $.ajax({
@@ -38,11 +38,31 @@ $(document).ready(function () {
             $('#trail3').attr('src',response.trails[2].imgSmallMed)
             $('#trail4').attr('src',response.trails[3].imgSmallMed)
             $('#trail5').attr('src',response.trails[4].imgSmallMed)
-            
+
+            //append results to cards?
+        
     })
     }
 
-// ----Weather API for retrieving longitude and latitude
+
+
+    
+//search event listener, changing certain elements to ids soon
+var savedPages =  JSON.parse(localStorage.getItem("searches")) || []; //may put this in a function to load saved pages
+
+$("button").on("click", function(event) {
+    event.preventDefault();
+    var searchInput = $("input").val().trim();
+
+    savedPages.push(searchInput); 
+    localStorage.setItem("searches").JSON.stringify(savedPages);
+
+    cityLocation(searchInput);//passing user input to cityLocation
+
+})
+
+
+// ----Weather API for retrieving longitude and latitude and pass to hiking project api
 function cityLocation(cityName) {
 
     var locationAPIKey = "&appid=0888bb26c1d027c60cb2417244156801";
@@ -57,12 +77,15 @@ function cityLocation(cityName) {
         var lat = response.coord.lat;
         console.log(lat);
         console.log(long);
+
+        //not needed for now, only need to pass lat & long
+        /*
         var divEl = $("<div>").addClass("coordinates")
         var locationLon = $("<p>").text("Longitude: " + long);
         var locationLat = $("<p>").text("Latitude: " + lat);
-    
         divEl.append(locationLon, locationLat);
-        getTrails(lat, long); //passing longitude and latitude parameters to getTrails
+        */
+        getTrails(lat, long); //passing longitude and latitude parameters to get trail results
     })
 }   
 
