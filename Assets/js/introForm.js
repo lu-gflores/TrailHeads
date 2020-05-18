38ba51229e5e40acd59f09aba2f9e6435cb2866e
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-05-18 01:17:13 -0400

    started combining swiper and form page set up saved button.

--- Assets/js/introForm.js
+++ Assets/js/introForm.js
@@ -1,4 +1,12 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  $("#zipDiv").hide();
+    $("#otherDiv").hide();
+    $(document).on("click", ".nextButton", function() {
+        event.preventDefault();
+        $(this).parent("div").next("div").show();
+        $(this).parent("div").hide();
+    })
+
     var mySwiper = new Swiper('.swiper-container', {
         loop: true,
         effect: 'coverflow',
@@ -28,7 +36,9 @@ $(document).ready(function () {
 
     function srcCheck() {
         if ($('.trail-img').attr('src') === '') {
-            $(this).attr('src', 'https://img.icons8.com/wired/64/000000/no-image.png')
+            $(this).html('<img src=https://www.freeiconspng.com/uploads/no-image-icon-7.gif')
+            
+           
         }
     }
 
@@ -38,44 +48,32 @@ $(document).ready(function () {
             url: 'https://www.hikingproject.com/data/get-trails?lat=35.9705&lon=-79.9974&maxDistance=10&key=200758271-3165bfaa7d6b0bedfbf0fcdfbad4ec16',
             method: 'GET'
         }).then(function (response) {
-            $('#trail1').attr('src', response.trails[0].imgSmallMed).attr('alt', 'trail')
-            $('#trail2').attr('src', response.trails[1].imgSmallMed).attr('alt', 'trail')
-            $('#trail3').attr('src', response.trails[2].imgSmallMed).attr('alt', 'trail')
-            $('#trail4').attr('src', response.trails[3].imgSmallMed).attr('alt', 'trail')
-            $('#trail5').attr('src', response.trails[4].imgSmallMed).attr('alt', 'trail')
+            $('#trail0').attr('src', response.trails[0].imgSmallMed).attr('alt', 'trail')
+            $('#trail1').attr('src', response.trails[1].imgSmallMed).attr('alt', 'trail')
+            $('#trail2').attr('src', response.trails[2].imgSmallMed).attr('alt', 'trail')
+            $('#trail3').attr('src', response.trails[3].imgSmallMed).attr('alt', 'trail')
+            $('#trail4').attr('src', response.trails[4].imgSmallMed).attr('alt', 'trail')
+            $('#slide0').text(response.trails[0].name)
+            $('#slide1').text(response.trails[1].name)
+            $('#slide2').text(response.trails[2].name)
+            $('#slide3').text(response.trails[3].name)
+            $('#slide4').text(response.trails[4].name)
             srcCheck();
             $('.save-trail').click(function () {
+                var n = $(this).attr('data-trail-index');
+                $('#slide'+n).text('SAVED').css('color','red')
                 trails.push(response.trails[$(this).attr('data-trail-index')])
                 localStorage.setItem('trails', JSON.stringify(trails))
+                
+                
             })
         })
     }
 $('#test').click(function(){
-    alert(localStorage.getItem('trails'))
+    $('#form-page').hide();
+    
 })
 
     getTrails();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 })
