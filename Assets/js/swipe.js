34aa09172ff57e1a886191ff8e773b9908b6571b
George Flores <georgeflores886@gmail.com>
2020-05-19 10:46:11 -0400

    fixed slider

--- Assets/js/swipe.js
+++ Assets/js/swipe.js
@@ -24,49 +24,58 @@ $(document).ready(function () {
       el: '.swiper-scrollbar',
     }
   })
+
   function generateExplorer(response) {
-      //div row, may not be needed for swipe
-      var divRow = $("<div>").addClass("flex flex-row justify-around my-16");
-      $("#container").append(divRow);//added id in html
-      var shadowElement = $("<div>").addClass("max-w-sm rounded overflow-hidden shadow-lg");
-      $(divRow).append(shadowElement);
-      var cardsArray = [];//store cards generated
-      //append results to cards/create new variables, subject to change.    
-      for (var i = 0; i < response.trails.length; i++) {
-        var divSlide = $('<div class="swiper-slide" id="slide">'+i);
+    //div row, may not be needed for swipe
+    var divRow = $("<div>").addClass("flex flex-row justify-around my-16");
+    $("#container").append(divRow);//added id in html
+    var shadowElement = $("<div>").addClass("max-w-sm rounded overflow-hidden shadow-lg");
+    $(divRow).append(shadowElement);
+    var cardsArray = [];//store cards generated
+    //append results to cards/create new variables, subject to change.    
+    for (var i = 0; i < response.trails.length; i++) {
+      var divSlide = $('<div class="swiper-slide" id="slide">' + i);
 
-        $(".swiper-wrapper").append(divSlide);
-        $('#slide'+ i).addClass();
+      $(".swiper-wrapper").append(divSlide);
+      $('#slide' + i).addClass();
 
-        var newCard = $("<div>").addClass("container flex flex-col items-center w-64 my-4");//trail card
-        divRow.append(newCard);
-        //image 
+      var newCard = $("<div>").addClass("container flex flex-col items-center w-64 my-4");//trail card
+      divRow.append(newCard);
+      //image 
+
+
+
+      if (response.trails[i].imgSmallMed === '') {
+        $('#slide' + i).html('<img src= "https://via.placeholder.com/150" alt = "no image">')
+      } else {
         var imgTag = $("<img>").addClass("w-full overflow-hidden h-64 overflow-hidden").attr("src", response.trails[i].imgSmallMed);
         newCard.append(imgTag);
+      }
 
-        var imageSlide = $("<img>").addClass("trail-img").attr('src', response.trails[i].imgSmallMed).attr('alt', 'trail');
-          divSlide.append(imageSlide);
+      
 
-        var cardBackground = $("<div>").addClass("px-4 py-4 bg-white");
-        //Trail Name
-        var trailName = $("<h1>").addClass("text-xl font-bold").text(response.trails[i].name);
-        newCard.append(trailName);
-        //div sub-container for summary paragraph
-        var divH = $("<div>").addClass("h-48");
-        //summary paragraph
-        var trailInfo = $("<p>").text(response.trails[i].summary);
-        divH.append(trailName, trailInfo);
-        cardBackground.append(divH);
-        newCard.append(cardBackground);
-        $("#slide"+i).append(divSlide);
 
-        cardsArray.push(newCard);
+      var cardBackground = $("<div>").addClass("px-4 py-4 bg-white");
+      //Trail Name
+      var trailName = $("<h1>").addClass("text-xl font-bold").text(response.trails[i].name);
+      newCard.append(trailName);
+      //div sub-container for summary paragraph
+      var divH = $("<div>").addClass("h-48");
+      //summary paragraph
+      var trailInfo = $("<p>").text(response.trails[i].summary);
+      divH.append(trailName, trailInfo);
+      cardBackground.append(divH);
+      newCard.append(cardBackground);
+      $("#slide" + i).append(divSlide);
 
+      divSlide.append(newCard);
+      //cardsArray.push(newCard);
+    }
   }
-  
+
   $('#slide').click(function () {
     cityLocation('Charlotte')
-    
+
     var mySwiper = new Swiper('.swiper-container', {
 
       loop: true,
@@ -101,7 +110,7 @@ $(document).ready(function () {
 
       localStorage.setItem("currentSearch", JSON.stringify(response));
       generateExplorer(response);
-      }
+    }
     )
   }
   //search event listener, changing certain elements to ids soon
@@ -122,6 +131,7 @@ $(document).ready(function () {
 
     }
   })
+
   // ----Weather API for retrieving longitude and latitude and pass to hiking project api
   function cityLocation(cityName, cityDistance) {
     var locationAPIKey = "&appid=0888bb26c1d027c60cb2417244156801";
@@ -133,9 +143,6 @@ $(document).ready(function () {
       var long = response.coord.lon;
       var lat = response.coord.lat;
       var temp = Math.round(response.main.temp)
-      // console.log(lat);
-      // console.log(long);
-      // console.log(temp);
 
       getTrails(lat, long, cityDistance, temp); //passing longitude and latitude parameters to get trail results
     })
@@ -152,4 +159,4 @@ $(document).ready(function () {
     var place = $(this).attr("data-name").replace(" ", "+");
     window.open("https://www.google.com/maps/search/?api=1&query=" + place)
   })
-})
+});
